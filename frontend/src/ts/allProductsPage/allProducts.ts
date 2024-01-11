417be67b41309fe51066c8f92e9253f871e79a5e
jess <jessica.response201@gmail.com>
2024-01-11 16:18:25 +0100

    dhffffffffffffffffff

--- frontend/src/ts/allProductsPage/allProducts.ts
+++ frontend/src/ts/allProductsPage/allProducts.ts
@@ -1,10 +1,15 @@
-import { getProducts } from '../functions/getProducts';
+
+
 import { oneProduct } from '../productPage/oneProduct';
 import { Product } from "../models/product";
+import productsfromjson from "../data/products.json";
 
 export const allProducts = async() => {
  
-     const productArray = await getProducts()
+
+
+
+     const productArray = [...productsfromjson]
     
 
 console.log(productArray)
