d40fae88d5645c29b34dc12b730e1114a83be119
jess <jessica.response201@gmail.com>
2024-01-11 16:01:28 +0100

    ytjjjjjjjjjjjjjjjjjjjjjj

--- frontend/src/ts/functions/getProducts.ts
+++ frontend/src/ts/functions/getProducts.ts
@@ -1,11 +1,38 @@
-
+import axios from "axios";
 import { Product } from "../models/product";
 import productsfromjson from "../data/products.json";
 
 export const getProducts = async () => {
 
-  const Array: Product[] =  [ ...productsfromjson]
+  const Array: Product[] = [];
+  try {
+    const { data } = await axios.get<Product[]>(`https://codecrafters-td9j.onrender.com`, {
+      timeout: 5000,
+    });
+    if (data.length >= 3) {
+      data.forEach((item) => {
+        Array.push(item);
+      });
+    } else {
+      if (productsfromjson) {
+        productsfromjson.forEach((item) => {
+          Array.push(item);
+        });
+      } else if (!productsfromjson) {
+        console.error("error");
+      }
+    }
+  } catch {
+    if (productsfromjson) {
+      productsfromjson.forEach((item) => {
+        Array.push(item);
+      });
+    } else {
+      console.error("error");
+    }
+  }
 
 
-  return Array
+  
+  return Array;
 };
