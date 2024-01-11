46ad289057f99c8b3d42150eee7fcb6e74260261
jess <jessica.response201@gmail.com>
2024-01-11 17:40:42 +0100

    dvsssssssssssssssss

--- frontend/src/ts/functions/getProducts.ts
+++ frontend/src/ts/functions/getProducts.ts
@@ -9,42 +9,6 @@ export const getProducts = async () => {
 
 
 
-const productsfromjson: Product[] = [ {
-  "_id": "658c0822982493ac38e5094f",
-  "titel": "Basic",
-  "image": "https://i.ibb.co/6bfc4bz/elsakaisa-close-up-eye-level-a-whiskey-with-orange-garnish-on-a-65febc51-13ab-41d1-b2d9-cfaf6fde748c.png",
-  "price": 1200,
-  "amount": 0,
-  "description": [
-      "Utforska grundläggande whiskyupplevelser med vårt prisvärda abonnemang. Smaka på noggrant utvalda destillerier och njut av varje droppe. Upptäck hemligheterna bakom varje butelj och låt din passion för whisky blomstra.",
-      "Upptäck en värld av rika smakupplevelser med vårt grundläggande whiskyabonnemang. Från rökiga torvtoner till söta sherry-nyanser, varje månad erbjuder vi en resa genom olika smakprofiler som kommer att förföra dina sinnen.",
-      "Utforska vårt grundläggande whiskyabonnemang och upplev bredden av våra partnerskap med kända viskemärken. Från de ikoniska skotska usen till innovativa artisanala destillerier, varje månad presenterar vi olika märken för att berika din samling."
-  ]
-},
-{
-  "_id": "658c09a4982493ac38e50950",
-  "titel": "Premium",
-  "image": "https://i.ibb.co/VSdnKGq/Microsoft-Teams-image-2.png",
-  "price": 1500,
-  "amount": 0,
-  "description": [
-      "Njut av exklusiva whiskyupplevelser med vårt premiumabonnemang. Varje månad presenterar vi exceptionella buteljer från sällsynta och eftertraktade destillerier. Upplev lyxen av unika smaker och sätt en extraordinär prägel på din whiskykollektion.",
-      "Förundras över en överlägsen smakresa med vårt premiumabonnemang. Från sällsynta single malts till exklusiva blandningar, varje månad väljer vi ut de finaste whiskysorterna för att erbjuda en oöverträffad smakupplevelse. Unna dig det bästa av det bästa.",
-      "Upptäck världen av lyxig whisky med vårt premiumabonnemang. Vi samarbetar med de mest prestigefyllda destillerierna och erbjuder en exklusiv samling varje månad. Ge din passion för whisky en upphöjd nivå med vårt noggrant kuraterade urval."
-  ]
-},
-{
-  "_id": "6590074fa26f7240140b969c",
-  "titel": "Collector's",
-  "image": "https://i.ibb.co/Fzr84SY/Microsoft-Teams-image-1.png",
-  "price": 2000,
-  "amount": 0,
-  "description": [
-      "För den seriösa samlaren erbjuder vårt Collector's-abonnemang en extraordinär inblick i whiskyns värld. Varje månad presenterar unika buteljer från historiska destillerier och begränsade upplagor. Skapa en samling av tidlös och exceptionell whisky.",
-      "Ge din samling en exklusiv touch med vårt Collector's-abonnemang. Vi kurerar buteljer från välrenommerade destillerier och whisky som är svår att hitta någon annanstans. Upplev spänningen av att äga verkliga juveler inom whiskyns värld.",
-      "Utforska sällsynta skatter med vårt Collector's-abonnemang. Varje månad tar vi dig med på en resa genom tid och tradition noggrant utvalda buteljer från legendariska destillerier. Skapa en unik och imponerande samling med varje leverans."
-  ]
-}]
 
 
 
@@ -54,7 +18,7 @@ const productsfromjson: Product[] = [ {
 
 
   const url: string = import.meta.env.VITE_URL;
-  const Array: Product[] = [];
+  let Array: Product[] = [];
   try {
     const { data } = await axios.get<Product[]>(`${url}`, {
       timeout: 500,
@@ -64,19 +28,87 @@ const productsfromjson: Product[] = [ {
         Array.push(item);
       });
     } else {
-      if (productsfromjson) {
-        productsfromjson.forEach((item) => {
-          Array.push(item);
-        });
-      } else if (!productsfromjson) {
-        console.error("error");
-      }
+      
+
+      Array = [ {
+        "_id": "658c0822982493ac38e5094f",
+        "titel": "Basic",
+        "image": "https://i.ibb.co/6bfc4bz/elsakaisa-close-up-eye-level-a-whiskey-with-orange-garnish-on-a-65febc51-13ab-41d1-b2d9-cfaf6fde748c.png",
+        "price": 1200,
+        "amount": 0,
+        "description": [
+            "Utforska grundläggande whiskyupplevelser med vårt prisvärda abonnemang. Smaka på noggrant utvalda destillerier och njut av varje droppe. Upptäck hemligheterna bakom varje butelj och låt din passion för whisky blomstra.",
+            "Upptäck en värld av rika smakupplevelser med vårt grundläggande whiskyabonnemang. Från rökiga torvtoner till söta sherry-nyanser, varje månad erbjuder vi en resa genom olika smakprofiler som kommer att förföra dina sinnen.",
+            "Utforska vårt grundläggande whiskyabonnemang och upplev bredden av våra partnerskap med kända viskemärken. Från de ikoniska skotska usen till innovativa artisanala destillerier, varje månad presenterar vi olika märken för att berika din samling."
+        ]
+      },
+      {
+        "_id": "658c09a4982493ac38e50950",
+        "titel": "Premium",
+        "image": "https://i.ibb.co/VSdnKGq/Microsoft-Teams-image-2.png",
+        "price": 1500,
+        "amount": 0,
+        "description": [
+            "Njut av exklusiva whiskyupplevelser med vårt premiumabonnemang. Varje månad presenterar vi exceptionella buteljer från sällsynta och eftertraktade destillerier. Upplev lyxen av unika smaker och sätt en extraordinär prägel på din whiskykollektion.",
+            "Förundras över en överlägsen smakresa med vårt premiumabonnemang. Från sällsynta single malts till exklusiva blandningar, varje månad väljer vi ut de finaste whiskysorterna för att erbjuda en oöverträffad smakupplevelse. Unna dig det bästa av det bästa.",
+            "Upptäck världen av lyxig whisky med vårt premiumabonnemang. Vi samarbetar med de mest prestigefyllda destillerierna och erbjuder en exklusiv samling varje månad. Ge din passion för whisky en upphöjd nivå med vårt noggrant kuraterade urval."
+        ]
+      },
+      {
+        "_id": "6590074fa26f7240140b969c",
+        "titel": "Collector's",
+        "image": "https://i.ibb.co/Fzr84SY/Microsoft-Teams-image-1.png",
+        "price": 2000,
+        "amount": 0,
+        "description": [
+            "För den seriösa samlaren erbjuder vårt Collector's-abonnemang en extraordinär inblick i whiskyns värld. Varje månad presenterar unika buteljer från historiska destillerier och begränsade upplagor. Skapa en samling av tidlös och exceptionell whisky.",
+            "Ge din samling en exklusiv touch med vårt Collector's-abonnemang. Vi kurerar buteljer från välrenommerade destillerier och whisky som är svår att hitta någon annanstans. Upplev spänningen av att äga verkliga juveler inom whiskyns värld.",
+            "Utforska sällsynta skatter med vårt Collector's-abonnemang. Varje månad tar vi dig med på en resa genom tid och tradition noggrant utvalda buteljer från legendariska destillerier. Skapa en unik och imponerande samling med varje leverans."
+        ]
+      }]
+
+
+
     }
   } catch {
     if (productsfromjson) {
-      productsfromjson.forEach((item) => {
-        Array.push(item);
-      });
+     Array = [ {
+      "_id": "658c0822982493ac38e5094f",
+      "titel": "Basic",
+      "image": "https://i.ibb.co/6bfc4bz/elsakaisa-close-up-eye-level-a-whiskey-with-orange-garnish-on-a-65febc51-13ab-41d1-b2d9-cfaf6fde748c.png",
+      "price": 1200,
+      "amount": 0,
+      "description": [
+          "Utforska grundläggande whiskyupplevelser med vårt prisvärda abonnemang. Smaka på noggrant utvalda destillerier och njut av varje droppe. Upptäck hemligheterna bakom varje butelj och låt din passion för whisky blomstra.",
+          "Upptäck en värld av rika smakupplevelser med vårt grundläggande whiskyabonnemang. Från rökiga torvtoner till söta sherry-nyanser, varje månad erbjuder vi en resa genom olika smakprofiler som kommer att förföra dina sinnen.",
+          "Utforska vårt grundläggande whiskyabonnemang och upplev bredden av våra partnerskap med kända viskemärken. Från de ikoniska skotska usen till innovativa artisanala destillerier, varje månad presenterar vi olika märken för att berika din samling."
+      ]
+    },
+    {
+      "_id": "658c09a4982493ac38e50950",
+      "titel": "Premium",
+      "image": "https://i.ibb.co/VSdnKGq/Microsoft-Teams-image-2.png",
+      "price": 1500,
+      "amount": 0,
+      "description": [
+          "Njut av exklusiva whiskyupplevelser med vårt premiumabonnemang. Varje månad presenterar vi exceptionella buteljer från sällsynta och eftertraktade destillerier. Upplev lyxen av unika smaker och sätt en extraordinär prägel på din whiskykollektion.",
+          "Förundras över en överlägsen smakresa med vårt premiumabonnemang. Från sällsynta single malts till exklusiva blandningar, varje månad väljer vi ut de finaste whiskysorterna för att erbjuda en oöverträffad smakupplevelse. Unna dig det bästa av det bästa.",
+          "Upptäck världen av lyxig whisky med vårt premiumabonnemang. Vi samarbetar med de mest prestigefyllda destillerierna och erbjuder en exklusiv samling varje månad. Ge din passion för whisky en upphöjd nivå med vårt noggrant kuraterade urval."
+      ]
+    },
+    {
+      "_id": "6590074fa26f7240140b969c",
+      "titel": "Collector's",
+      "image": "https://i.ibb.co/Fzr84SY/Microsoft-Teams-image-1.png",
+      "price": 2000,
+      "amount": 0,
+      "description": [
+          "För den seriösa samlaren erbjuder vårt Collector's-abonnemang en extraordinär inblick i whiskyns värld. Varje månad presenterar unika buteljer från historiska destillerier och begränsade upplagor. Skapa en samling av tidlös och exceptionell whisky.",
+          "Ge din samling en exklusiv touch med vårt Collector's-abonnemang. Vi kurerar buteljer från välrenommerade destillerier och whisky som är svår att hitta någon annanstans. Upplev spänningen av att äga verkliga juveler inom whiskyns värld.",
+          "Utforska sällsynta skatter med vårt Collector's-abonnemang. Varje månad tar vi dig med på en resa genom tid och tradition noggrant utvalda buteljer från legendariska destillerier. Skapa en unik och imponerande samling med varje leverans."
+      ]
+    }]
+    
     } else {
       console.error("error");
     }
