814daae18b0493ccd2974d83a7c4ee981ea67b84
Jessica <114924033+Response201@users.noreply.github.com>
2024-01-14 22:59:06 +0100

    dcscd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -29,13 +29,22 @@ export default defineConfig({
   // Konfigurera byggnadsutdata
   build: {
     outDir: 'dist', // Byggmappen
-    assetsDir: 'public', // Mappen där assets (till exempel bilder) lagras
+    assetsDir: '', // Mappen där assets (till exempel bilder) lagras
+    sourcemap: false, // Skapa eller inte skapa källkartor
+    
+
 
     rollupOptions: {
       input: {
         main: 'index.html', // Huvud-HTML-fil
         about: 'about.html', // Lägg till andra HTML-filer här
-      },
+      }
+  }
+
+
+
+
+
   },
 
   // Plugins och andra konfigurationer kan läggas till här
