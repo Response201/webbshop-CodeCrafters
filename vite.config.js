a2eaf80be7997c81897ea384581990e337429f6e
Jessica <114924033+Response201@users.noreply.github.com>
2024-01-14 21:41:22 +0100

    gregr

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
 
   build: {
     // Ange output-mappen för byggresultaten
-   /*  outDir: 'dist', */
+    outDir: 'dist',
 
     // Ange formatet för JavaScript-moduler
     /* target: 'esnext', */
