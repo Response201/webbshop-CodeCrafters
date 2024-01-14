e5bfbe1f213a7f25cf05e77d000057c9b65e0c01
Jessica <114924033+Response201@users.noreply.github.com>
2024-01-14 22:12:33 +0100

    dvsv

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,30 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // ... övriga konfigurationsinställningar
+  // Bas URL för produktion eller utveckling
+  base: '/',
 
+  // Om du har flera sidor i ditt projekt, definiera dem här
+  // Mer information: https://vitejs.dev/config/#pages
+  // pages: {
+  //   index: 'src/index.html',
+  //   about: 'src/about.html',
+  //   // ...
+  // },
+
+  // Ange om du vill att Vite ska visa en notifiering om fel
+  // Mer information: https://vitejs.dev/config/#server-quiet
+  server: {
+    quiet: false,
+  },
+
+  // Konfigurera byggnadsutdata
   build: {
-    // Ange output-mappen för byggresultaten
-    base: '/',
-
-    // Ange formatet för JavaScript-moduler
-    /* target: 'esnext', */
-
     outDir: 'dist', // Byggmappen
     assetsDir: '', // Mappen där assets (till exempel bilder) lagras
     sourcemap: false, // Skapa eller inte skapa källkartor
   },
-    /*   base: './' */
 
-
-    
-
-    // Om du har flera sidor i ditt projekt, definiera dem här
-    // Mer information: https://vitejs.dev/config/#pages
-    // pages: {
-    //   index: 'src/index.html',
-    //   about: 'src/about.html',
-    //   // ...
-    // },
-  
-    // Ange om du vill att Vite ska visa en notifiering om fel
-    // Mer information: https://vitejs.dev/config/#server-quiet
-    server: {
-      quiet: false,
-    },
-  
-    // Konfigurera byggnadsutdata
-    
+  // Plugins och andra konfigurationer kan läggas till här
 });
