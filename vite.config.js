2229e90063473b7d01dff7ff90a6a85fbfe63eed
Jessica <114924033+Response201@users.noreply.github.com>
2024-01-14 22:11:22 +0100

    sdfdsfd

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,34 @@ export default defineConfig({
 
   build: {
     // Ange output-mappen för byggresultaten
-    outDir: 'dist',
+    base: '/',
 
     // Ange formatet för JavaScript-moduler
     /* target: 'esnext', */
 
-    
+    outDir: 'dist', // Byggmappen
+    assetsDir: '', // Mappen där assets (till exempel bilder) lagras
+    sourcemap: false, // Skapa eller inte skapa källkartor
+  },
     /*   base: './' */
+
+
     
 
-  },
+    // Om du har flera sidor i ditt projekt, definiera dem här
+    // Mer information: https://vitejs.dev/config/#pages
+    // pages: {
+    //   index: 'src/index.html',
+    //   about: 'src/about.html',
+    //   // ...
+    // },
+  
+    // Ange om du vill att Vite ska visa en notifiering om fel
+    // Mer information: https://vitejs.dev/config/#server-quiet
+    server: {
+      quiet: false,
+    },
+  
+    // Konfigurera byggnadsutdata
+    
 });
