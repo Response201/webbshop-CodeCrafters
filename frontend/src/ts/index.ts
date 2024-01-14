eace151a7b0682499930692e5e4e0cf58f296d3f
Jessica <114924033+Response201@users.noreply.github.com>
2024-01-14 19:47:05 +0100

    fvvfd

--- frontend/src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-import { allProducts } from "./allProductsPage/allProducts";
-import { getProducts } from "./functions/getProducts";
-import { Main } from "./main";
-
-
-
-Main()
-getProducts()
-allProducts()
